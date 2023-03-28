eb5721e3f02dfe9d2a30904c8965bca6f7c706b7
chethanpoojar <chethanpn032@gmail.com>
2023-03-28 18:14:15 +0530

    copying value to excel sheet at last row

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import MoveJsonToExcel from "./module/pages/fileUpload";
 const App = (e) => {
   const [sourcesheets, setsourcesheets] = useState();
   const [jsonData, setjsonData] = useState({});
+  const [modifiedJsonData, setModifiedJsonData] = useState({});
+  const [excelFile, setExcelFile] = useState(null);
 
-  console.log(jsonData, "newJsonDatanewJsonData");
+  console.log(modifiedJsonData, "modifiedJsonData");
+  console.log(jsonData, "jsonData");
 
   function get_name(name, lis) {
     let aa = [];
@@ -64,7 +67,12 @@ const App = (e) => {
           value !== undefined
         ) {
           out_dic = get_dict(key, value, out_dic);
-        } else if (typeof value === "string") {
+        } else if (
+          typeof value === "string" ||
+          typeof value === "number" ||
+          typeof value === null ||
+          typeof value === undefined
+        ) {
           key = get_sec_name(key, out_dic);
           if (out_dic[name]) {
             out_dic[name][key] = value;
@@ -77,111 +85,76 @@ const App = (e) => {
     return out_dic;
   }
 
-  useEffect(() => {
-    let aa = get_dict("Root", json, {});
-    setjsonData(aa);
-  }, []);
-
-  const hadleFileInput = (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-      const data = e.target.result;
-      const wb = XLSX.read(data, { type: "binary" });
-      setsourcesheets(wb.SheetNames);
-
-      for (let sheetname in jsonData) {
-        const ws = wb.Sheets[`(REQ)_${sheetname}`];
-        const range = XLSX.utils.decode_range(ws["!ref"]);
-        range.s.r = 4;
-        range.e.r = 4;
-        const headers = [];
-        for (let C = range.s.c; C <= range.e.c; C++) {
-          for (let column in jsonData[sheetname]) {
-            const cellAddress = XLSX.utils.encode_cell({
-              r: range.s.r,
-              c: C,
-            });
-            if (!ws[cellAddress] || !ws[cellAddress].v) continue;
-            headers.push(ws[cellAddress].v);
-            ws[cellAddress].v = "A";
-          }
-        }
-
-        console.log(headers, "headers");
-      }
-
-      const outputData = XLSX.write(wb, {
-        type: "binary",
-        bookType: "xlsx",
-      });
-      const blob = new Blob([s2ab(outputData)], {
-        type: "application/octet-stream",
-      });
-      saveAs(blob, file.name);
-    };
-
-    reader.readAsBinaryString(e.target.files[0]);
+  const json = {
+    name: "John Smith",
+    age: 30,
+    email: "john.smith@example.com",
+    hobbies: ["reading", "traveling", "photography"],
   };
 
-  const sourceFileInput = (event) => {
-    const file = event.target.files[0];
+  // useEffect(() => {
+  //   let aa = get_dict("Root", json, {});
+  //   setModifiedJsonData(aa);
+  // }, []);
+
+  const handleJsonFileChange = (e) => {
+    const file = e.target.files[0];
+    setjsonData(file);
+    const reader = new FileReader();
+    reader.onload = () => {
+      const data = JSON.parse(reader.result);
+      let aa = get_dict("Root", data, {});
+      setModifiedJsonData(aa);
+    };
+    reader.readAsText(file);
+  };
+
+  const handleExcelFileChange = (e) => {
+    const file = e.target.files[0];
+    setExcelFile(file);
+  };
+
+  const handleUpload = (event) => {
     const reader = new FileReader();
 
     reader.onload = function (e) {
       const data = e.target.result;
       const workbook = XLSX.read(data, { type: "binary" });
-      console.log(workbook.SheetNames, "workbook");
       setsourcesheets(workbook.SheetNames);
 
-      for (let sheetname in jsonData) {
+      for (let sheetname in modifiedJsonData) {
         const worksheet = workbook.Sheets[`(REQ)_${sheetname}`];
-        const range = XLSX.utils.decode_range(worksheet["!ref"]);
-        range.s.r = 4;
-        range.e.r = 4;
-        // Iterate through the cells in the first row and extract the column names
-        const headers = [];
-        for (let C = range.s.c; C <= range.e.c; ++C) {
-          for (let column in jsonData[sheetname]) {
-            const address = XLSX.utils.encode_cell({ r: range.s.r, c: C });
-            let cell = worksheet[address];
-            if (!cell || !cell.v) continue;
-            headers.push(cell.v);
-            const cellAddress = XLSX.utils.encode_cell({ r: range.s.r, c: C });
 
-            // range.s.r = 5;
-            // range.e.r = 5;
-            // for (let C = range.s.c; C <= range.e.c; ++C) {
-            //   if (cell.v == column) {
-            //     const columnIndex = headers.indexOf(cell.v);
-            //     const rowIndex = 5;
-            //     const cellAddress = XLSX.utils.encode_cell({
-            //       r: rowIndex,
-            //       c: columnIndex,
-            //     });
-            //     const cell = worksheet[cellAddress];
-            //     if (!cell || !cell.v) continue;
-            //     if (worksheet[cellAddress]) {
-            //       worksheet[cellAddress].v = jsonData[sheetname][column] ?? "A";
-            //     }
-            //   }
-            // }
+        // Get the current highest row number in the sheet
+        const currentHighestRow = worksheet["!ref"]
+          .split(":")[1]
+          .replace(/\D/g, "");
 
-            // range.s.r = 5;
-            // range.e.r = 5;
-            // console.log(cell.v, ":", column, "cell column");
-            // for (let C = range.s.c; C <= range.e.c; ++C) {
-            //   // if (cell.v == column) {
-            //   const address = XLSX.utils.encode_cell({ r: range.s.r, c: C });
-            //   let cell = worksheet[address];
-            //   if (!cell || !cell.v) continue;
-            //   cell.v =  "A";
-            //   // }
-            // }
-          }
-        }
-        console.log(headers, "headers");
+        // Insert a new row after the current highest row
+        const newRowNumber = parseInt(currentHighestRow) + 1;
+        const newRowRef = `A${newRowNumber}:Z${newRowNumber}`;
+        XLSX.utils.sheet_add_aoa(worksheet, [[""]], {
+          origin: newRowRef,
+        });
+
+        const headerRow = XLSX.utils.sheet_to_json(worksheet, { header: 1 })[4];
+        const jsonKeys = Object.keys(modifiedJsonData[sheetname]);
+
+        jsonKeys.forEach((key) => {
+          const columnIndex = headerRow.findIndex((header) =>
+            key.match(new RegExp(header + "*"))
+          );
+          // if (columnIndex !== -1) {
+          // Write the value of the key in the corresponding cell of the 5th row
+          const cellAddress = XLSX.utils.encode_cell({
+            r: newRowNumber - 1,
+            c: columnIndex,
+          });
+          worksheet[cellAddress] = {
+            v: modifiedJsonData[sheetname][key] ?? "A",
+          };
+          // }
+        });
       }
 
       const outputData = XLSX.write(workbook, {
@@ -191,10 +164,10 @@ const App = (e) => {
       const blob = new Blob([s2ab(outputData)], {
         type: "application/octet-stream",
       });
-      saveAs(blob, file.name);
+      saveAs(blob, excelFile.name);
     };
 
-    reader.readAsBinaryString(event.target.files[0]);
+    reader.readAsBinaryString(excelFile);
   };
 
   const s2ab = (s) => {
@@ -205,9 +178,21 @@ const App = (e) => {
   };
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", margin: "auto" }}>
-      <input type="file" onChange={sourceFileInput} accept=".xlsx" />
-      {sourcesheets?.length > 0 && (
+    <div
+      style={{ display: "flex", justifyContent: "center", marginTop: "4rem" }}
+    >
+      <input type="file" onChange={handleJsonFileChange} />
+
+      <input type="file" onChange={handleExcelFileChange} accept=".xlsx" />
+
+      <button
+        disabled={!(modifiedJsonData && excelFile)}
+        onClick={handleUpload}
+      >
+        Upload
+      </button>
+
+      {/* {sourcesheets?.length > 0 && (
         <div style={{ marginTop: "3rem" }}>
           {" "}
           Sheets :
@@ -217,39 +202,12 @@ const App = (e) => {
             })}
           </select>
         </div>
-      )}
-
-      {/* <button onClick={""}>Generate JSON</button> */}
-
-      {/* <MoveJsonToExcel /> */}
+      )} */}
     </div>
   );
 };
 
 export default App;
-
-const json = {
-  name: "John Smith",
-  age: 30,
-  email: "john.smith@example.com",
-  hobbies: ["reading", "traveling", "photography"],
-};
-
-const json2 = {
-  PartnerAccountInquiryReqDTO: {
-    searchCriteria: "1",
-    maskedAccountNumber: "1382",
-    mobileNumber: "9885804000",
-  },
-  sessionContext: {
-    channel: "MB53",
-    bankCode: "08",
-    userId: "DevUser01",
-    externalReferenceNo: "1234565",
-    transactionBranch: "089999",
-    transactingPartyCode: "9898765645",
-  },
-};
 
 const JSON_DATA = {
   UserData: "UserData",
@@ -393,124 +351,4 @@ const JSON_DATA = {
     },
   },
   Culture: "en-US",
-};
-
-const MODIFIED_JSON = {
-  root: {
-    UserData: "UserData",
-    Culture: "en-US",
-  },
-  RequestInfo: {
-    SolutionSetName: "Go_KOTAK_MAHINDRA_AGSS",
-    ExecuteLatestVersion: true,
-  },
-  Service: {
-    Skip: "N",
-    Id: "ComBS",
-    GSTStateCode: "07",
-    Consent: "true",
-  },
-  Operation: {
-    Name09: "CommercialBureau",
-  },
-  Param: {
-    Value: "100000",
-    Name: "Amount",
-  },
-  Param02: {
-    Value02: "true",
-    Name02: "CIRHTMLReport",
-  },
-  Param03: {
-    Value03: "true",
-    Name03: "MSMERank",
-  },
-  Param04: {
-    Value04: "JSON",
-    Name04: "ResponseType",
-  },
-  Param05: {
-    Value05: "03300001",
-    Name05: "MemberCode",
-  },
-  Param06: {
-    Value06: "BP",
-    Name06: "MemberKOB",
-  },
-  Param07: {
-    Value07: "BP03300001_CMMUATC2C",
-    Name07: "UserId",
-  },
-  Param08: {
-    Value08: "y9vpaa@jbyWadjm",
-    Name08: "UserPassword",
-  },
-  Param09: {
-    Value09: "20022020",
-    Name10: "DateOfRegistration",
-  },
-  Param10: {
-    Value10: "MANUFACTURE OF DOMESTIC APPLIANCES",
-    Name11: "ClassOfActivity",
-  },
-  Param11: {
-    Value11: "NOT CLASSIFIED",
-    Name12: "TypeOfEntity",
-  },
-  Param12: {
-    Value12: "11452675",
-    Name13: "MemberReferenceNumber",
-  },
-  Param13: {
-    Value13: "Overdraft",
-    Name14: "FacilityCategory",
-  },
-  Param14: {
-    Value14: "NEW LOAN",
-    Name15: "FacilityType",
-  },
-  Applicant: {
-    TIN: "",
-    PAN: "AAACJ9928K",
-    CRN: null,
-    CompanyName: "MS JORJY INTERNATIONAL PVT LTD",
-    CIN: "",
-    ApplicantType: "Company",
-  },
-  Service02: {
-    Id: "ComBS",
-  },
-  Operation02: {
-    Name16: "CommercialBureau",
-  },
-  RegisteredAddress: {
-    AddressType: "REGISTERED",
-    AddressStateCode: "07",
-    AddressPincode: "110037",
-    AddressLine3: "Third cross",
-    AddressLine2: "Second main",
-    AddressLine1: "First line",
-    AddressCity: "SOUTH WEST DELHI",
-  },
-  Telephone: {
-    TelephoneType: "OFFICE",
-    Telephone_Num: "7973124432",
-    Contact_Prefix: null,
-    Contact_Area: null,
-  },
-  OtherAddress: {
-    AddressType02: "Others",
-    AddressStateCode02: "07",
-    AddressPincode02: "110037",
-    AddressLine302: "Third cross",
-    AddressLine202: "Second main",
-    AddressLine102: "First line",
-    AddressCity02: "SOUTH WEST DELHI",
-  },
-  Telephone02: {
-    TelephoneType02: "OFFICE",
-    Telephone_Num02: "7973124432",
-    Contact_Prefix02: null,
-    Contact_Area02: null,
-  },
 };
